fa27821d91f15af18473dd982f7844df1cf408a5
k-yumin <kyumnkm2@gmail.com>
2024-11-27 21:32:00 +0900

    ShadowJar with Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "2.0.21"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 group = "yt.yacht"
@@ -17,6 +20,23 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("shadow")
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "yt.yacht.MainKt"))
+        }
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
 kotlin {
     jvmToolchain(21)
 }
